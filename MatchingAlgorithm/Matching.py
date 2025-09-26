360571f762d1ea64e0345dc75032c4dae2070363
Benjamin wasisco <bwasisco@eecs.wsu.edu>
2025-09-26 12:49:24 -0700

    Adding comments

--- MatchingAlgorithm/Matching.py
+++ MatchingAlgorithm/Matching.py
@@ -1,30 +1,52 @@
 import math as m
-# Dot Product Function
+
+# Finds dot prod. between 2 vectors
 def FindDotProduct (p1 : list, p2 : list):
+    """
+    Calculates the dot product between two given vectors
+    Requires two lists as input
+    """
+
     if (len(p1) == len(p2)):
         sum = 0
+
         for i, j in zip(p1, p2):
             sum += i*j
-    else:
-       return "Vectors not same length"
 
-# Find Unit Vector
+        return sum
+    else:
+       return 2.0
+
+
+# Finds unit vector
 def FindUnitVector (v : list):
+    """
+    Calculates the unit vector for a given vector.
+    Requires a list as input
+    """
+
     res = []
     magnitude = 0
 
-    #
+    # Finding vector magnitude.
     for i in v:
         magnitude += m.pow(i,2)
     magnitude = m.sqrt(magnitude)
 
-    #
+    # Normalizing the vector.
     for i in v:
         res.append(i/magnitude)
 
     return res
 
+# Determines if a matching is stable.
 def StableMatch (dotProd : float):
+    """
+    Determines if two roommates would be a stable match
+    Requires a float as input
+    """
+
     if (dotProd > .7):
         return True
+    
     return False
