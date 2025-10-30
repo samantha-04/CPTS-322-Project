25058d74aaaf91e4939645faed47d08ed025e26f
samantha-04 <samanthaklein680@gmail.com>
2025-10-30 13:24:55 -0700

    adding user.py

---
+++ code/backend/User.py
@@ -0,0 +1,15 @@
+class User:
+
+    AGREE_DISAGREE = {
+        'Strongly Disagree' : -1.0,
+        'Disagree': -0.5,
+        'Neutral' : 0,
+        'Agree': 0.5,
+        'Strongly Agree': 1.0
+    }
+
+    YES_NO = {
+        'Yes' : 1.0,
+        'No': -1.0
+    }
+
